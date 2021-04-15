6789e1ab160560f1dc4e4a683a844baff748892d
Farkhod Alimov <farhod.alimov@gmail.com>
2021-04-15 00:03:37 -0600

    Consolidating headers setup logic for FileService

--- src/app/services/file.service.ts
+++ src/app/services/file.service.ts
@@ -7,41 +7,36 @@ import { environment } from "../../environments/environment";
 })
 export class FileService {
   private baseApiUrl: string = environment.baseApiUri;
+  private headers: HttpHeaders = null;
 
-  constructor(private http: HttpClient) {}
-
-  loadFiles() {
-    const headers = new HttpHeaders({
+  constructor(private http: HttpClient) {
+    this.headers = new HttpHeaders({
       Authorization: `Bearer ${localStorage.getItem("auth-token")}`,
     });
+  }
+
+  loadFiles() {
     const params = new HttpParams({
       fromObject: { page: "1", perPage: "100" },
     });
     const uri = `${this.baseApiUrl}/files/list`;
-    return this.http.get(uri, { headers: headers, params: params });
+    return this.http.get(uri, { headers: this.headers, params: params });
   }
 
   downloadFile(fileName: string) {
-    const headers = new HttpHeaders({
-      Authorization: `Bearer ${localStorage.getItem("auth-token")}`,
-    });
     const params = new HttpParams({
       fromObject: { filename: fileName },
     });
     const uri = `${this.baseApiUrl}/files/download`;
     return this.http.get(uri, {
-      headers: headers,
+      headers: this.headers,
       params: params,
       responseType: "blob",
     });
   }
 
   uploadFile(data: FormData) {
-    const headers = new HttpHeaders({
-      Authorization: `Bearer ${localStorage.getItem("auth-token")}`,
-    });
-
     const uri = `${this.baseApiUrl}/files/upload`;
-    return this.http.post(uri, data, { headers: headers });
+    return this.http.post(uri, data, { headers: this.headers });
   }
 }
